4d6d36c4fbedd3e7bf13fbb6c16c79a08c9edfbc
Andres Pegam <apegam@pro-vision.de>
2017-04-24 17:39:11 +0200

    better error message on Galen spec failures

--- galenium/src/main/java/io/wcm/qa/galenium/testcase/AbstractGaleniumInteractiveBaseTestCase.java
+++ galenium/src/main/java/io/wcm/qa/galenium/testcase/AbstractGaleniumInteractiveBaseTestCase.java
@@ -152,7 +152,7 @@ public abstract class AbstractGaleniumInteractiveBaseTestCase extends AbstractGa
       GalenLayoutChecker.handleLayoutReport(layoutReport, errorMessage, successMessage);
     }
     catch (Throwable ex) {
-      fail("layout check failed.", ex);
+      fail(errorMessage, ex);
     }
   }
 
