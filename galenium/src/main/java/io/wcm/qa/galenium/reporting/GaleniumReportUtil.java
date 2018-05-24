3d1fb6d3d7466986cbba50e8e7080e73dab54f59
Andres Pegam <apegam@pro-vision.de>
2018-05-24 15:44:53 +0200

    do not add empty tags

--- galenium/src/main/java/io/wcm/qa/galenium/reporting/GaleniumReportUtil.java
+++ galenium/src/main/java/io/wcm/qa/galenium/reporting/GaleniumReportUtil.java
@@ -148,6 +148,10 @@ public final class GaleniumReportUtil {
    * @param category to add
    */
   public static void assignCategory(String category) {
+    if (StringUtils.isBlank(category)) {
+      // do not tag blank categories
+      return;
+    }
     ExtentTest extentTest = getExtentTest();
     List<TestAttribute> categoryList = extentTest.getTest().getCategoryList();
     for (TestAttribute testAttribute : categoryList) {
