afc4703b229609c1df3cc1075b9d5c869b00b697
Andres Pegam <apegam@pro-vision.de>
2018-08-27 08:27:08 +0200

    click methods for selectors matching multiple elements

--- modules/interaction/src/main/java/io/wcm/qa/galenium/interaction/Mouse.java
+++ modules/interaction/src/main/java/io/wcm/qa/galenium/interaction/Mouse.java
@@ -33,6 +33,7 @@ import org.openqa.selenium.interactions.Actions;
 
 import com.galenframework.browser.SeleniumBrowser;
 
+import io.wcm.qa.galenium.exceptions.GaleniumException;
 import io.wcm.qa.galenium.selectors.base.Selector;
 
 /**
@@ -42,6 +43,33 @@ public final class Mouse {
 
   private Mouse() {
     // do not instantiate
+  }
+
+  /**
+   * Click at current position.
+   */
+  public static void click() {
+    getLogger().debug("Clicking at current position.");
+    getActions().click().perform();
+  }
+
+  /**
+   * Click at location of element. Useful when encountering 'other element would receive click' exceptions.
+   * @param selector identifies element
+   */
+  public static void clickLocation(Selector selector) {
+    clickLocation(selector, 0);
+  }
+
+  /**
+   * Click at location of nth element defined by selector. Useful when encountering 'other element would receive click'
+   * exceptions.
+   * @param selector identifies elements
+   * @param index which of the elements
+   */
+  public static void clickLocation(Selector selector, int index) {
+    moveTo(selector, index);
+    click();
   }
 
   /**
@@ -66,10 +94,20 @@ public final class Mouse {
    */
   public static void hover(Selector selector) {
     getLogger().debug("attempting mouse over: " + selector.elementName());
+    int index = 0;
+    hover(selector, index);
+  }
+
+  /**
+   * Hover mouse over element over nth element defined by selector.
+   * @param selector identifies elements
+   * @param index identifies which of the elements
+   */
+  public static void hover(Selector selector, int index) {
     WebDriver driver = getDriver();
     List<WebElement> mouseOverElements = driver.findElements(selector.asBy());
     if (!mouseOverElements.isEmpty()) {
-      WebElement mouseOverElement = mouseOverElements.get(0);
+      WebElement mouseOverElement = mouseOverElements.get(index);
       if (mouseOverElement.isDisplayed()) {
         try {
           moveTo(selector);
@@ -90,6 +128,15 @@ public final class Mouse {
   }
 
   /**
+   * Move horizontally.
+   * @param horizontalOffset
+   * @param verticalOffset
+   */
+  public static void moveByOffset(int horizontalOffset, int verticalOffset) {
+    getActions().moveByOffset(horizontalOffset, verticalOffset).perform();
+  }
+
+  /**
    * Move mouse horizontally over page and scroll if necessary to keep it in viewport.
    * @param horizontalOffset to move mouse horizontally by (negative values move to the left.
    */
@@ -105,39 +152,38 @@ public final class Mouse {
   }
 
   /**
-   * Move horizontally.
-   * @param horizontalOffset
-   * @param verticalOffset
+   * Move mouse pointer to element.
+   * @param selector identifies element
    */
-  public static void moveByOffset(int horizontalOffset, int verticalOffset) {
-    getActions().moveByOffset(horizontalOffset, verticalOffset).perform();
+  public static void moveTo(Selector selector) {
+    moveTo(selector, 0);
   }
 
   /**
    * Move mouse pointer to element.
    * @param selector identifies element
+   * @param index which of the elements
    */
-  public static void moveTo(Selector selector) {
-    WebElement element = Element.findOrFail(selector);
+  public static void moveTo(Selector selector, int index) {
+    List<WebElement> elements = Element.findAll(selector);
+    if (elements.isEmpty()) {
+      throw new GaleniumException("when moving to '" + selector + "': no elements found");
+    }
+    if (elements.size() <= index) {
+      StringBuilder message = new StringBuilder()
+          .append("when moving to '")
+          .append(selector)
+          .append("[")
+          .append(index)
+          .append("]: only found ")
+          .append(elements.size())
+          .append(" elements, but needed ")
+          .append(index + 1);
+      throw new GaleniumException(message.toString());
+    }
+    WebElement element = elements.get(index);
     getLogger().debug("Moving to element: " + element);
     getActions().moveToElement(element).perform();
-  }
-
-  /**
-   * Click at location of element. Useful when encountering 'other element would receive click' exceptions.
-   * @param selector identifies element
-   */
-  public static void clickLocation(Selector selector) {
-    moveTo(selector);
-    click();
-  }
-
-  /**
-   * Click at current position.
-   */
-  public static void click() {
-    getLogger().debug("Clicking at current position.");
-    getActions().click().perform();
   }
 
   private static Actions getActions() {
