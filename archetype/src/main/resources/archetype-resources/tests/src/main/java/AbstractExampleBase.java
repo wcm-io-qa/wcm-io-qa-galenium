0ed9c1e3a2dd91c0fecf21eabb2aac517835bbee
Andres Pegam <apegam@pro-vision.de>
2018-10-19 17:35:25 +0200

    formatting

--- archetype/src/main/resources/archetype-resources/tests/src/main/java/AbstractExampleBase.java
+++ archetype/src/main/resources/archetype-resources/tests/src/main/java/AbstractExampleBase.java
@@ -6,13 +6,11 @@ import io.wcm.qa.galenium.testcase.AbstractGaleniumBase;
 /**
  * Abstract base class for common functionality needed by multiple tests.
  */
-public class AbstractExampleBase extends AbstractGaleniumBase
-{
+public class AbstractExampleBase extends AbstractGaleniumBase {
     /**
      * @param testDevice test device to use for test
      */
     public AbstractExampleBase(TestDevice testDevice) {
         super(testDevice);
-
     }
 }
