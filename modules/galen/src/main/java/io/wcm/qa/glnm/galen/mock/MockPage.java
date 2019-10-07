333eee6ebab13c3232c1f0b75642796e936af048
Andres Pegam <apegam@pro-vision.de>
2019-10-07 16:22:58 +0200

    refactor: move Galen mocks to Galen module

--- modules/galen/src/main/java/io/wcm/qa/glnm/galen/mock/MockPage.java
+++ modules/galen/src/main/java/io/wcm/qa/glnm/galen/mock/MockPage.java
@@ -17,7 +17,7 @@
  * limitations under the License.
  * #L%
  */
-package io.wcm.qa.glnm.maven.mock;
+package io.wcm.qa.glnm.galen.mock;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
