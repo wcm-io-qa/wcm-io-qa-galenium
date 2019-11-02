bd1aff2b4f30ca382f70de61969d92c25cc7ecd6
Andres Pegam <apegam@pro-vision.de>
2019-11-02 02:51:58 +0100

    feat: add new types to encapsulate Galen
    Galen API is quite vast. For Galenium we want to use only
    parts of it.

---
+++ modules/galen/src/main/java/io/wcm/qa/glnm/galen/GalenSpec.java
@@ -0,0 +1,56 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2019 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.qa.glnm.galen;
+
+import com.galenframework.specs.page.PageSpec;
+
+import io.wcm.qa.glnm.galen.validation.GalenValidation;
+
+/**
+ * Provide access to everything about this Galen spec.
+ */
+public class GalenSpec {
+
+  private PageSpec pageSpec;
+  private GalenSpecProvider galenSpecProvider;
+
+  /**
+   * Run the spec.
+   * @return representation of the run
+   */
+  public GalenSpecRun check() {
+    return new GalenSpecRun(this, GalenValidation.check(getPageSpec()));
+  }
+
+  protected PageSpec getPageSpec() {
+    if (pageSpec == null) {
+      pageSpec = initPageSpec();
+    }
+    return pageSpec;
+  }
+
+  protected PageSpec initPageSpec() {
+    return getPageSpecProvider().getPageSpec();
+  }
+
+  private GalenSpecProvider getPageSpecProvider() {
+    return galenSpecProvider;
+  }
+}
---
+++ modules/galen/src/main/java/io/wcm/qa/glnm/galen/GalenSpecRun.java
@@ -0,0 +1,85 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2019 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.qa.glnm.galen;
+
+import java.util.List;
+
+import com.galenframework.reports.model.LayoutReport;
+import com.galenframework.validation.ValidationResult;
+
+/**
+ * Represents an execution of a Galen spec.
+ */
+public class GalenSpecRun {
+
+  private LayoutReport report;
+  private GalenSpec spec;
+
+  GalenSpecRun(GalenSpec spec, LayoutReport report) {
+    this.setSpec(spec);
+    this.setReport(report);
+  }
+
+  /**
+   * When there are errors that are not marked as warning level only.
+   * @return whether spec has failed
+   */
+  public boolean hasFailed() {
+    return getReport().errors() > 0;
+  }
+
+  /**
+   * When there are errors that are not marked as warning level only.
+   * @return whether spec has failed
+   */
+  public boolean hasWarnings() {
+    return getReport().warnings() > 0;
+  }
+
+
+  /**
+   * When there are no errors or warnings the spec is considered clean.
+   * @return whether spec is clean
+   */
+  public boolean isClean() {
+    return getValidationErrors().isEmpty();
+  }
+
+  private List<ValidationResult> getValidationErrors() {
+    return getReport().getValidationErrorResults();
+  }
+
+  protected LayoutReport getReport() {
+    return report;
+  }
+
+  protected GalenSpec getSpec() {
+    return spec;
+  }
+
+  protected void setReport(LayoutReport report) {
+    this.report = report;
+  }
+
+  protected void setSpec(GalenSpec spec) {
+    this.spec = spec;
+  }
+
+}
