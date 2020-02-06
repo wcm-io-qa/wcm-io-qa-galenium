f3fb75b202a08ddca3db468df85d8bc72fbdad53
Andres Pegam <apegam@pro-vision.de>
2020-02-06 09:53:05 +0100

    feat(examples): add test example for generic status test

---
+++ examples/tests/src/test/java/io/wcm/qa/glnm/example/generic/ResponseStatusIT.java
@@ -0,0 +1,49 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2020 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.qa.glnm.example.generic;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.seljup.Options;
+import io.wcm.qa.glnm.hamcrest.GaleniumAssert;
+import io.wcm.qa.glnm.hamcrest.LogEntryMatchers;
+import io.wcm.qa.glnm.interaction.Browser;
+import io.wcm.qa.glnm.interaction.Wait;
+import io.wcm.qa.glnm.junit.CheckResponseCodes;
+import io.wcm.qa.glnm.junit.ChromePerformanceLog;
+
+public class ResponseStatusIT implements CheckResponseCodes {
+
+  /**
+   * Chrome options with performance logging will be used when initializing chrome driver.
+   */
+  @Options
+  ChromeOptions chromeOptions = ChromePerformanceLog.chromeOptionsWithPerformanceLog().setHeadless(true);
+
+
+  @Test
+  void checkResponses() {
+    Browser.load("https://pro-vision.de");
+    Wait.forDomReady(5);
+    GaleniumAssert.assertResponses(Matchers.everyItem(LogEntryMatchers.hasStatus(200)));
+  }
+}
