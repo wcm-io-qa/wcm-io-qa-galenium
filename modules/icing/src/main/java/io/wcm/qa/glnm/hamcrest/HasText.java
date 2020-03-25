6ca9cfae1946ac5e1f0747fb99d47e8d808b4ba5
Andres Pegam <apegam@pro-vision.de>
2020-03-25 19:27:54 +0100

    feat(hamcrest): add more selector matchers
    Attribute, text, and visibility with no direct baseline.

---
+++ modules/icing/src/main/java/io/wcm/qa/glnm/hamcrest/HasText.java
@@ -0,0 +1,55 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2020 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.qa.glnm.hamcrest;
+
+import org.hamcrest.Matcher;
+
+import io.wcm.qa.glnm.sampling.element.TextSampler;
+import io.wcm.qa.glnm.selectors.base.Selector;
+
+/**
+ * Matches if element defined by selector has a text matched by
+ * underlying matcher.
+ *
+ * @since 5.0.0
+ */
+public class HasText extends SelectorSamplerMatcher<String> {
+
+  /**
+   * <p>Constructor for HasText.</p>
+   *
+   * @param matcher to match elements text
+   */
+  public HasText(Matcher<String> matcher) {
+    super(matcher, TextSampler.class);
+  }
+
+  /**
+   * Matches if element defined by selector has a text matched by
+   * underlying matcher.
+   *
+   * @param matcher to match element's text
+   * @return matcher retrieving text
+   */
+  public static Matcher<Selector> hasText(Matcher<String> matcher) {
+    return new HasText(matcher);
+  }
+
+}
