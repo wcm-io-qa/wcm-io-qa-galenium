db199a022f573711dca05c2aa759c5b551fb78e5
Andres Pegam <apegam@pro-vision.de>
2020-03-25 19:27:54 +0100

    feat(hamcrest): add more selector matchers
    Attribute, text, and visibility with no direct baseline.

---
+++ modules/icing/src/main/java/io/wcm/qa/glnm/hamcrest/HasAttribute.java
@@ -0,0 +1,54 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2020 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.qa.glnm.hamcrest;
+
+import org.hamcrest.Matcher;
+
+import io.wcm.qa.glnm.sampling.element.AttributeSampler;
+import io.wcm.qa.glnm.selectors.base.Selector;
+
+/**
+ * Matches if selector defines a visible element.
+ *
+ * @since 5.0.0
+ */
+public class HasAttribute extends SelectorSamplerMatcher<String> {
+
+  /**
+   * Constructor.
+   *
+   * @param attributeName a {@link java.lang.String} object.
+   * @param matcher a {@link org.hamcrest.Matcher} object.
+   */
+  public HasAttribute(String attributeName, Matcher<String> matcher) {
+    super(matcher, AttributeSampler.class, attributeName);
+  }
+
+  /**
+   * Is element defined by selector visible
+   *
+   * @return matcher
+   * @param name a {@link java.lang.String} object.
+   * @param matcher a {@link org.hamcrest.Matcher} object.
+   */
+  public static Matcher<Selector> hasAttribute(String name, Matcher<String> matcher) {
+    return new HasAttribute(name, matcher);
+  }
+}
