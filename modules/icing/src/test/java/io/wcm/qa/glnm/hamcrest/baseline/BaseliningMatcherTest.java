534d9d50d6226d995a29b174205c87b73d1445b8
Andres Pegam <apegam@pro-vision.de>
2020-04-22 16:14:49 +0200

    fix: remove unused import

--- modules/icing/src/test/java/io/wcm/qa/glnm/hamcrest/baseline/BaseliningMatcherTest.java
+++ modules/icing/src/test/java/io/wcm/qa/glnm/hamcrest/baseline/BaseliningMatcherTest.java
@@ -26,7 +26,6 @@ import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import io.wcm.qa.glnm.hamcrest.baseline.BaselineMatchers;
 import io.wcm.qa.glnm.persistence.BaselinePersistenceExtension;
 
 @ExtendWith(BaselinePersistenceExtension.class)
