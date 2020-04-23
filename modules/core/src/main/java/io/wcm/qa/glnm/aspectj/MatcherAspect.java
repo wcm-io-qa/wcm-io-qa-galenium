672ff74656fe817971a7664bd615984a720b80ae
Andres Pegam <apegam@pro-vision.de>
2020-04-24 00:53:43 +0200

    fix: fix scope of aspect and general clean up

--- modules/core/src/main/java/io/wcm/qa/glnm/aspectj/MatcherAspect.java
+++ modules/core/src/main/java/io/wcm/qa/glnm/aspectj/MatcherAspect.java
@@ -12,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.wcm.qa.glnm.reporting.GaleniumReportUtil;
-
 /**
  * <p>
  * MatcherAspect class.
@@ -20,10 +19,9 @@ import io.wcm.qa.glnm.reporting.GaleniumReportUtil;
  *
  * @since 5.0.0
  */
-@Aspect("perthis(execution(boolean org.hamcrest.Matcher.matches(Object)))")
+@Aspect("perthis(execution(* org.hamcrest..*(..)))")
 public class MatcherAspect {
 
-  private static final String HAMCREST_MATCHER_MATCH = "hamcrestMatcherMatch()";
   private static final Logger LOG = LoggerFactory.getLogger(MatcherAspect.class);
   private String startStepUuid;
 
@@ -46,7 +44,9 @@ public class MatcherAspect {
    */
   @Before("execution(boolean org.hamcrest.Matcher.matches(Object))")
   public void startMatching(final JoinPoint joinPoint) {
-    LOG.info(joinPoint.getSignature().toLongString());
+    if (LOG.isTraceEnabled()) {
+      LOG.trace("before: " + joinPoint.getSignature().toLongString());
+    }
     startStepUuid = GaleniumReportUtil.startStep("matching");
   }
 
@@ -56,8 +56,11 @@ public class MatcherAspect {
    * @param joinPoint a {@link org.aspectj.lang.JoinPoint} object.
    * @param e a {@link java.lang.Throwable} object.
    */
-  @AfterThrowing(value = "execution(boolean org.hamcrest.Matcher.matches(Object))", throwing = "e")
+  @AfterThrowing(pointcut = "execution(boolean org.hamcrest.Matcher.matches(Object))", throwing = "e")
   public void failedMatching(final JoinPoint joinPoint, final Throwable e) {
+    if (LOG.isTraceEnabled()) {
+      LOG.trace("failed: " + joinPoint.getSignature().toLongString());
+    }
     StringDescription stringDescription = new StringDescription();
     Matcher target = (Matcher)joinPoint.getTarget();
     target.describeTo(stringDescription);
@@ -73,6 +76,9 @@ public class MatcherAspect {
    */
   @AfterReturning(pointcut = "execution(boolean org.hamcrest.Matcher.matches(Object))", returning = "result")
   public void doneMatching(final JoinPoint joinPoint, boolean result) {
+    if (LOG.isTraceEnabled()) {
+      LOG.trace("done: " + joinPoint.getSignature().toLongString());
+    }
     Matcher target = (Matcher)joinPoint.getTarget();
     StringDescription stringDescription = new StringDescription();
     target.describeTo(stringDescription);
@@ -82,6 +88,7 @@ public class MatcherAspect {
       GaleniumReportUtil.stopStep();
     }
     else {
+      stringDescription.appendText("\n");
       target.describeMismatch(joinPoint.getArgs()[0], stringDescription);
       GaleniumReportUtil.updateStepName(startStepUuid, stringDescription.toString());
       GaleniumReportUtil.failStep(startStepUuid);
