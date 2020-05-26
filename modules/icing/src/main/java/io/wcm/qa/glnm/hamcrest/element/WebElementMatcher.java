fb562b74cba1c77899efa436091ab36c1f1d6613
Andres Pegam <apegam@pro-vision.de>
2020-05-26 13:50:25 +0200

    feat(hamcrest): add new WebElement matchers
    Most values can be accessed with Hamcrest HasProperty, but
    CSS and attribute need arguments.

---
+++ modules/icing/src/main/java/io/wcm/qa/glnm/hamcrest/element/WebElementMatcher.java
@@ -0,0 +1,70 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2020 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.qa.glnm.hamcrest.element;
+
+import java.util.function.Function;
+
+import org.apache.commons.lang3.StringUtils;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.openqa.selenium.WebElement;
+
+import io.wcm.qa.glnm.hamcrest.FunctionalWrappingMatcher;
+
+
+class WebElementMatcher<T> extends FunctionalWrappingMatcher<WebElement, T> {
+
+  private String mappingDescription;
+
+  protected WebElementMatcher(
+      String description,
+      Function<WebElement, T> mapping,
+      Matcher<T> matcher) {
+    super(mapping, matcher);
+    setMappingDescription(description);
+  }
+
+  @Override
+  public void describeTo(Description description) {
+    if (StringUtils.isNotEmpty(getMappingDescription())) {
+      description.appendText(getMappingDescription());
+      description.appendText(" ");
+    }
+    super.describeTo(description);
+  }
+
+  protected String getMappingDescription() {
+    return mappingDescription;
+  }
+
+  protected void setMappingDescription(String mappingDescription) {
+    this.mappingDescription = mappingDescription;
+  }
+
+  /**
+   * @param <T> type of mapped value
+   * @param mapping extracting value from element
+   * @param matcher matches mapped value
+   * @return matcher doing mapping and matching
+   */
+  public static <T> Matcher<WebElement> elementMapped(Function<WebElement, T> mapping, Matcher<T> matcher) {
+    return new WebElementMatcher<T>("", mapping, matcher);
+  }
+}
