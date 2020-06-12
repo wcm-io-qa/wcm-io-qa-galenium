469d7bf23f062a99119e8eae650d1a288d397240
Andres Pegam <apegam@pro-vision.de>
2020-06-12 13:34:38 +0200

    feat: add annotations to declare non-argument sources
    Provider still need to be implemented. Plan is to go with Extensions, but
    API is deliberately loose on types.

---
+++ modules/core/src/main/java/io/wcm/qa/glnm/junit/combinatorial/CombinableSource.java
@@ -0,0 +1,45 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2020 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.qa.glnm.junit.combinatorial;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Support for non-argument sources for combinatorial tests.
+ *
+ * @since 5.0.0
+ */
+@Retention(RUNTIME)
+@Target({ METHOD, ANNOTATION_TYPE })
+@Repeatable(CombinableSources.class)
+public @interface CombinableSource {
+
+  /**
+   * @return type of combinable provider
+   */
+  Class<CombinableProvider> value();
+
+}
