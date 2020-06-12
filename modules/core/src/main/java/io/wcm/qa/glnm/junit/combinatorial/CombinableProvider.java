f456a31bc15d7c79f3feb4c7db94d427111050af
Andres Pegam <apegam@pro-vision.de>
2020-06-12 13:34:38 +0200

    feat: add annotations to declare non-argument sources
    Provider still need to be implemented. Plan is to go with Extensions, but
    API is deliberately loose on types.

---
+++ modules/core/src/main/java/io/wcm/qa/glnm/junit/combinatorial/CombinableProvider.java
@@ -0,0 +1,44 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2020 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.qa.glnm.junit.combinatorial;
+
+import java.util.List;
+
+/**
+ * Provides combinables.
+ *
+ * @since 5.0.0
+ */
+public interface CombinableProvider {
+
+  /**
+   * <p>combinables.</p>
+   *
+   * @return combinable inputs for combinatorial tests
+   */
+  List<Combinable> combinables();
+
+  /**
+   * Used for filtering.
+   *
+   * @return type of provided combinable value
+   */
+  Class providedType();
+}
